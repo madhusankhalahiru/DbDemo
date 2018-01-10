6730f7a1ff1059cb0bde2db333c7752094718391
Lahiru madhusankha gamage <madhusankhalahiru@gmail.com>
2018-01-10 08:26:12 +0530

    Initial commit

---
+++ src/main/java/com/dbexample/DbDemo/DbDemoApplication.java
@@ -0,0 +1,12 @@
+package com.dbexample.DbDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/dbexample/DbDemo/Repository.java
@@ -0,0 +1,6 @@
+package com.dbexample.DbDemo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface Repository extends CrudRepository<User, Long> {
+
+}
